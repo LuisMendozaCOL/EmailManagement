c555e046e7f5997fdd1cb9786ec1c89de98fc0f5
LuisMendozaCol <lamendozac@unal.edu.co>
2022-11-08 15:39:56 -0500

    MPV 1

--- main.py
+++ main.py
@@ -4,59 +4,70 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from template import set_name
+import csv
 
-subject = "An email with attachment from Python"
-body = '''
-Buenas tardes
+def send_mail(name, receiver_email):
+    sender_email = "lamendozac@unal.edu.co"
+    receiver_email = "lamendozac@unal.edu.co"
+    #password = input("Type your password and press enter:")
+    subject = "Vacante: desarrollador en Python"
 
-Srs. PEER
+    # Create a multipart message and set headers
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
+    message["Bcc"] = receiver_email  # Recommended for mass emails
 
-Les escribo con el fin de informarles que me gustaría conocer la vacante para desarrollador en Python de su empresa.
+    # Set name within both plain/html templates
+    template_text, template_html = set_name(name)
+    
+    # Turn body into plain/html MIMEText objects
+    part1 = MIMEText(template_text, "plain")
+    part2 = MIMEText(template_html, "html")
 
-Adjunto a este correo envío mi hoja de vida en inglés y español, según se requiera.
+    # Add HTML/plain-text parts to MIMEMultipart message
+    # The email client will try to render the last part first
+    #message.attach(part1)
+    message.attach(part2)
 
-Atento a sus comentarios me despido.
+    filename = "files/1067957571_CVR_English_24Oct2022.pdf"  # In same directory as script
 
-Saludos,
-'''
-sender_email = "lamendozac@unal.edu.co"
-receiver_email = "lamendozac@unal.edu.co"
-#password = input("Type your password and press enter:")
+    # Open PDF file in binary mode
+    with open(filename, "rb") as attachment:
+        # Add file as application/octet-stream
+        # Email client can usually download this automatically as attachment
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
 
-# Create a multipart message and set headers
-message = MIMEMultipart()
-message["From"] = sender_email
-message["To"] = receiver_email
-message["Subject"] = subject
-message["Bcc"] = receiver_email  # Recommended for mass emails
+    # Encode file in ASCII characters to send by email    
+    encoders.encode_base64(part)
 
-# Add body to email
-message.attach(MIMEText(body, "plain"))
+    # Add header as key/value pair to attachment part
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {filename}",
+    )
 
-filename = "Attachment.pdf"  # In same directory as script
+    # Add attachment to message and convert message to string
+    message.attach(part)
+    #text = message.as_string()
 
-# Open PDF file in binary mode
-with open(filename, "rb") as attachment:
-    # Add file as application/octet-stream
-    # Email client can usually download this automatically as attachment
-    part = MIMEBase("application", "octet-stream")
-    part.set_payload(attachment.read())
+    # Log in to server using secure context and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(user, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+    
 
-# Encode file in ASCII characters to send by email    
-encoders.encode_base64(part)
+def run():
+    with open("emails.csv") as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip header row
+        for name, email in reader:
+            print(f"Sending email to {name}")
+            send_mail(name, email)
 
-# Add header as key/value pair to attachment part
-part.add_header(
-    "Content-Disposition",
-    f"attachment; filename= {filename}",
-)
-
-# Add attachment to message and convert message to string
-message.attach(part)
-text = message.as_string()
-
-# Log in to server using secure context and send email
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(user, password)
-    server.sendmail(sender_email, receiver_email, text)
+if __name__ == '__main__':
+    run()
---
+++ template.py
@@ -0,0 +1,178 @@
+def set_name(name):
+    template_html = f'''
+<p><span>Muy buenas tardes</span><br /><br /><span>Apreciados Srs. {name}</span><br /><br /><span>Cordial saludo,</span><br /><br /><span>De la forma m&aacute;s respuetuosa les escribo para comunicarles mi inter&eacute;s en la vacante disponible en su empresa para el cargo de desarrolador en Python.</span><br /><br /><span>Adjunto a este correo env&iacute;o mi hoja&nbsp;de vida en ingl&eacute;s y espa&ntilde;ol, seg&uacute;n&nbsp;se requiera.</span><br /><br /><span>Antes de despedirme le informo que este correo fue enviado utilizando la aplicaci&oacute;n desarrolada por mi persona como parte de los proyectos de formaci&oacute;n en Platzi. Puede acceder al mismo a trav&eacute;s de GitHub,&nbsp;<a href="https://github.com/LuisMendozaCOL/EmailManagement">LuisMendozaCOL/EmailManagement (github.com)</a></span><br /><br /><span>Atento a sus comentarios me despido.</span><br /><br /><span>Saludos,</span></p>
+<p><span></span></p>
+<p><span></span></p>
+
+<table cellpadding="0" cellspacing="0"
+    style="color: #ffffff; font-size: medium; vertical-align: -webkit-baseline-middle; font-family: Arial;">
+    <tbody>
+        <tr>
+            <td>
+                <table cellpadding="0" cellspacing="0"
+                    style="vertical-align: -webkit-baseline-middle; font-family: Arial;">
+                    <tbody>
+                        <tr>
+                            <td style="vertical-align: top;">
+                                <table cellpadding="0" cellspacing="0"
+                                    style="vertical-align: -webkit-baseline-middle; font-family: Arial;">
+                                    <tbody>
+                                        <tr>
+                                            <td height="30"></td>
+                                        </tr>
+                                        <tr>
+                                            <td style="text-align: center;">
+                                                <table cellpadding="0" cellspacing="0"
+                                                    style="vertical-align: -webkit-baseline-middle; font-family: Arial; display: inline-block;">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td><a href="https://www.facebook.com/LuchoMendozaC"
+                                                                    color="#6A78D1"
+                                                                    style="display: inline-block; padding: 0px; background-color: #6a78d1;"
+                                                                    target="_blank"
+                                                                    data-saferedirecturl="https://www.google.com/url?q=https://www.facebook.com/LuchoMendozaC&amp;source=gmail&amp;ust=1668019170468000&amp;usg=AOvVaw20L5vwFL6PX4VTmbpD8Ab8"
+                                                                    rel="noopener"><img
+                                                                        src="https://ci6.googleusercontent.com/proxy/CsU8Viqi3BJDAFLrGZPksmkYgWVO33uPMuUPYTdIjlZGkYPTUoI_vJDzFKjQFwApPgNeOzuP2McTvftBr9y45oU4K7hT_3YVrqR7L-3VwYbeIS13VrCdWig_8JnKG5CZ_mBs7omd-uFCFStjfCVo=s0-d-e1-ft#https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/facebook-icon-2x.png"
+                                                                        alt="facebook" color="#6A78D1" height="24"
+                                                                        style="max-width: 135px; display: block;"
+                                                                        class="CToWUd" data-bit="iit" /></a></td>
+                                                            <td width="5">
+                                                                <div></div>
+                                                            </td>
+                                                            <td><a href="https://www.linkedin.com/in/luis-armando-mendoza-campillo-3184b7160/"
+                                                                    color="#6A78D1"
+                                                                    style="display: inline-block; padding: 0px; background-color: #6a78d1;"
+                                                                    target="_blank"
+                                                                    data-saferedirecturl="https://www.google.com/url?q=https://www.linkedin.com/in/luis-armando-mendoza-campillo-3184b7160/&amp;source=gmail&amp;ust=1668019170468000&amp;usg=AOvVaw1WpwGhOR57m-seLgyhivtr"
+                                                                    rel="noopener"><img
+                                                                        src="https://ci6.googleusercontent.com/proxy/8VnMNxHLCZ0mb5p6kFUeerh69ZxNFn796FO-bPB4zCIy6zKpR1zhFWOLua5F0V0VgIit8AVUmjEgifJrk7e9BwF3wOGdMevsrii7gV2oBOFEo5guBdtnCAwg1eRcW3MR-HHxsstpA8fhJPI5apj8=s0-d-e1-ft#https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/linkedin-icon-2x.png"
+                                                                        alt="linkedin" color="#6A78D1" height="24"
+                                                                        style="max-width: 135px; display: block;"
+                                                                        class="CToWUd" data-bit="iit" /></a></td>
+                                                            <td width="5">
+                                                                <div></div>
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                            <td width="46">
+                                <div></div>
+                            </td>
+                            <td style="padding: 0px; vertical-align: middle;">
+                                <h3 color="#000000" style="margin: 0px; font-size: 18px; color: #000000;"><span
+                                        style="font-weight: normal;">Luis Armando</span><span
+                                        style="font-weight: normal;">&nbsp;</span><span
+                                        style="font-weight: normal;">Mendoza Campillo</span></h3>
+                                <p color="#000000"
+                                    style="color: #000000; margin: 0px; font-size: 14px; line-height: 22px;">Ingeniero
+                                    de Petr&oacute;leos</p>
+                                <p color="#000000"
+                                    style="color: #000000; margin: 0px; font-size: 14px; line-height: 22px;">Universidad
+                                    Nacional de Colombia - sede Medell&iacute;n</p>
+                                <table cellpadding="0" cellspacing="0"
+                                    style="vertical-align: -webkit-baseline-middle; font-family: Arial; width: 289px;">
+                                    <tbody>
+                                        <tr>
+                                            <td height="30"></td>
+                                        </tr>
+                                        <tr>
+                                            <td color="#0b79ec" height="1"
+                                                style="width: 289px; border-bottom: 1px solid #0b79ec; border-left: none; display: block;">
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td height="30"></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <table cellpadding="0" cellspacing="0"
+                                    style="vertical-align: -webkit-baseline-middle; font-family: Arial;">
+                                    <tbody>
+                                        <tr height="25" style="vertical-align: middle;">
+                                            <td width="30" style="vertical-align: middle;">
+                                                <table cellpadding="0" cellspacing="0"
+                                                    style="vertical-align: -webkit-baseline-middle; font-family: Arial;">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td style="vertical-align: bottom;"><span color="#0b79ec"
+                                                                    width="11"
+                                                                    style="display: block; background-color: #0b79ec;"><img
+                                                                        src="https://ci6.googleusercontent.com/proxy/Xq3hntJEq2rjJzR0uWCVm3clsSla7NsI7xyRuy0B6esGxKEs0TJKSCBJd0PTJnw80_-gOm3yRwJoGtSWipm4TqjnmSCEllHm6WPq2oze68mmA8DO6Mj2dGBHroByKflVGCBL0c-wyQ3vCF92=s0-d-e1-ft#https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/phone-icon-2x.png"
+                                                                        color="#0b79ec" width="13"
+                                                                        style="display: block;" class="CToWUd"
+                                                                        data-bit="iit" /></span></td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                            <td style="padding: 0px; color: #000000;"><a href="tel:3003567066"
+                                                    color="#000000" style="color: #000000; font-size: 12px;"
+                                                    target="_blank" rel="noopener">3003567066</a></td>
+                                        </tr>
+                                        <tr height="25" style="vertical-align: middle;">
+                                            <td width="30" style="vertical-align: middle;">
+                                                <table cellpadding="0" cellspacing="0"
+                                                    style="vertical-align: -webkit-baseline-middle; font-family: Arial;">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td style="vertical-align: bottom;"><span color="#0b79ec"
+                                                                    width="11"
+                                                                    style="display: block; background-color: #0b79ec;"><img
+                                                                        src="https://ci5.googleusercontent.com/proxy/u9Dqq8IRTYcA9pxGhij8X1100IBTEBNk6GfgLex2wy5mIUGt4EvtpI__1csTElV-MUMrqJCa2SjWZkRDmYNbTv260GIk6RQb8BWD6Fub4s38olgLolJ-Y0ZMzSkDaCxhCmOgByGso4GxlMz7=s0-d-e1-ft#https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/email-icon-2x.png"
+                                                                        color="#0b79ec" width="13"
+                                                                        style="display: block;" class="CToWUd"
+                                                                        data-bit="iit" /></span></td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                            <td style="padding: 0px;"><a href="mailto:lcampillo2@slb.com"
+                                                    color="#000000" style="color: #000000; font-size: 12px;"
+                                                    target="_blank" rel="noopener">lamendozac@unal.edu.co</a></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <table cellpadding="0" cellspacing="0"
+                                    style="vertical-align: -webkit-baseline-middle; font-family: Arial;">
+                                    <tbody>
+                                        <tr>
+                                            <td height="30"></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+    </tbody>
+</table>
+'''
+
+    template_text = f'''
+Muy buenas tardes
+
+Apreciados Srs. {name}
+
+Cordial saludo,
+
+De la forma más respuetuosa les escribo para comunicarles mi interés en la vacante disponible en su empresa para el cargo de desarrolador en Python.
+
+Adjunto a este correo envío mi hoja de vida en inglés y español, según se requiera.
+
+Antes de despedirme le informo que este correo fue enviado utilizando la aplicación desarrolada por mi persona, como parte de los proyectos de formación en Platzi. Puede acceder al mismo a través de GitHub (https://github.com/LuisMendozaCOL/EmailManagement)
+
+Atento a sus comentarios me despido.
+
+Saludos,
+
+Luis Armando Mendoza Campillo
+Ingeniero de Petróleos / Desarrollador en Python
+'''
+    return template_text, template_html
